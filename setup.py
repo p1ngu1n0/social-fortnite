86272f90e6f02712734dc0cd231586a6dbcecc79
p1ngu1n0 <36717227+p1ngu1n0@users.noreply.github.com>
2019-01-15 21:28:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import subprocess
 
 b = subprocess.getoutput("find $HOME -type d -name SocialFish ")
 
-ds = "/base/WebPages/Fortnite"
+ds = "/base/WebPages/fortnite"
 lugar = str(b+ds)
 print("Instalando en "+lugar)
 move = os.system("cp -r fortnite "+lugar)
